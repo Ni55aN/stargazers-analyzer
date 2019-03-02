a980832a7cc0bba93e79756c065108d7d3ec2770
Vitaliy Stoliarov <huccah@gmail.com>
2019-03-02 20:56:36 +0200

    sync DB connection

--- src/db.js
+++ src/db.js
@@ -1,15 +1,10 @@
 import Dexie from 'dexie';
-let db = null;
+let db = new Dexie("github-stars");
 
-export default async function openDB() {
-    if(!db) {
-        db = new Dexie("github-stars");
+db.version(1).stores({
+    users: "&login,email",
+    stargazers: "[login+repo],repo"
+});
 
-        db.version(1).stores({
-            users: "&login,email",
-            stargazers: "[login+repo],repo"
-        });
-    }
-  
-    return db;
-}
+
+export default db;
