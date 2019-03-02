e9465e8b7d265cbf96b1150ae056c6e9d3a3fad1
Vitaliy Stoliarov <huccah@gmail.com>
2019-03-02 20:48:39 +0200

    badge

---
+++ src/components/Badge.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+Badge.badge(:count="count", :overflow-count="overflowCount")
+  slot
+</template>
+
+<script>
+export default {
+  props: {
+    count: Number,
+    overflowCount: {
+      type: Number,
+      default: 20000
+    }
+  }
+}
+</script>
+
+
+<style lang="sass" scoped>
+.badge
+  margin: 2em 1em
+  color: #7b7b7b
+  background: #e8e8e8
+  border-radius: 6px
+  padding: 0.5em
+  display: inline-block
+</style>
