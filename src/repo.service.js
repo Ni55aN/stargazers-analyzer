c20cf92f8ba73139df32347aa79437c1d2c3e56d
Ni55aN <huccah@gmail.com>
2023-04-30 22:31:52 +0300

    fix non-resume load

--- src/repo.service.js
+++ src/repo.service.js
@@ -3,37 +3,37 @@ import db from './db';
 import { loadStargazers, loadRepo, loadUser } from './loader';
 
 export default class {
-    constructor() {
-      this.name = '';
-      this.data = {};
+  constructor() {
+    this.name = '';
+    this.data = {};
+    this.users = [];
+    Vue.observable(this)
+  }
+
+  async setRepo(name) {
+    this.name = name;
+    this.users = await db.stargazers.where('repo').equals(this.name).toArray();
+    this.data = await loadRepo(name);
+  }
+
+  async loadStars(resume) {
+    let page = 1;
+    if (resume) {
+      const count = await db.stargazers.where('repo').equals(this.name).count();
+
+      page = Math.floor(count / 30) + 1;
+    } else {
+      await db.stargazers.where('repo').equals(this.name).delete();
       this.users = [];
-      Vue.observable(this)
     }
 
-    async setRepo(name) {
-        this.name = name;
-        this.users = await db.stargazers.where('repo').equals(this.name).toArray();
-        this.data = await loadRepo(name);
-    }
+    await loadStargazers(this.name, page, async (star) => {
+      const data = { repo: this.name, login: star.login };
 
-    async loadStars(resume) {
-        let page = 1;
-        if(resume) {
-          const count = await db.stargazers.where('repo').equals(this.name).count();
-  
-          page = Math.floor(count/30) + 1;
-        } else {
-          await db.stargazers.where('repo').equals(this.name).delete();
-          this._e.users = [];
-        }
-
-        await loadStargazers(this.name, page, async (star) => {
-            const data = { repo: this.name, login: star.login };
-      
-            if(!await db.stargazers.get(data)) {
-              await db.stargazers.add(data);
-              this.users.push(star);
-            }
-        })
-    }
+      if (!await db.stargazers.get(data)) {
+        await db.stargazers.add(data);
+        this.users.push(star);
+      }
+    })
+  }
 }
